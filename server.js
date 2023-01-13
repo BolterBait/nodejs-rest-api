f9e36b7f2cc45766601c571f113ffbad595a98c3
BolterBait <osvyatobog@gmail.com>
2023-01-13 20:00:31 +0200

    start app

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const { app } = require('./app')
+const dotenv = require('dotenv');
 
-app.listen(3001, () => {
-  console.log("Server running. Use our API on port: 3001")
-})
+dotenv.config();
+mongoose.set('strictQuery', false);
+
+const { HOST_URI } = process.env;
+async function main() {
+  try {
+    await mongoose.connect(HOST_URI);
+
+    console.log("Database connection successful");
+    app.listen(3001, () => {
+      console.log("Server running. Use our API on port: 3001")
+    });
+  } catch (error) {
+    console.error('Main failed', error.message)
+    process.exit(1);
+  }
+}
+main();
+
+
