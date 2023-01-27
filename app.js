1ec0b3e4d0b07e19fc34438ade85390fdcb6868e
osvyatobog <osvyatobog@gmail.com>
2023-01-27 15:41:38 +0200

    public dir

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
 const contactsRouter = require('./routes/api/contacts');
 const { authRouter } = require('./routes/api/auth');
 const { usersRouter } = require('./routes/api/users');
-const { filesRouter } = require('./routes/api/files');
 
+const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-const app = express()
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/upload', filesRouter);
+app.use('/public', express.static('public'));
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((error, req, res, next) => {
-
-  if (error.message.includes("Cast to ObjectId failed for value")) {
-    console.error(error)
-    return res.status(400).json({ message: "id is invalid" })
+  if (error.message.includes('Cast to ObjectId failed for value')) {
+    console.error(error);
+    return res.status(400).json({ message: 'id is invalid' });
   }
   // if (error.status) {
   //   return res.status(error.status || 500).json({ message: error.message || "Internal server error" })
   // }
   // console.error(error)
-  return res.status(400).json({message: error})
+  return res.status(400).json({ message: error });
 });
 
-module.exports = { app, };
+module.exports = { app };
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,61 +1,97 @@
 const { validateBody } = require('../middlewares/validator');
 const { Contact } = require('../models/index');
+const fs = require('fs/promises');
+const path = require('path');
 
 async function getContacts(req, res, next) {
-    const { limit = 100, page = 1, favorite } = req.query;
-    const skip = (page - 1) * limit;
+  const { limit = 100, page = 1, favorite } = req.query;
+  const skip = (page - 1) * limit;
 
-    if(favorite){
-        const favoriteContacts = await Contact.find({favorite: true})
-        return res.status(200).json(favoriteContacts);
-    }
-    const contacts = await Contact.find({}).skip(skip).limit(limit);
-    return res.status(200).json({ contacts })
+  if (favorite) {
+    const favoriteContacts = await Contact.find({ favorite: true });
+    return res.status(200).json(favoriteContacts);
+  }
+  const contacts = await Contact.find({}).skip(skip).limit(limit);
+  return res.status(200).json({ contacts });
 }
 
 async function getContactById(req, res, next) {
-    const { contactId } = req.params;
-    const contact = await Contact.findById(contactId);
-    if (!contact) {
-        return res.status(404).json({ message: 'Not found' });
-    }
-    return res.json({ contact });
-};
+  const { contactId } = req.params;
+  const contact = await Contact.findById(contactId);
+  if (!contact) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+  return res.json({ contact });
+}
 async function createContact(req, res, next) {
-    const {_id} = req.params;
-    const {name, email, phone, favorite } =req.body;
-    const validatedData = validateBody(req.body);
-    if (validatedData.error) {
-        return res.status(400).json({ status: validatedData.error })
-    }
-    const newContact = await Contact.create({
-        name, email, phone, favorite, owner: _id,
-    });
-    return res.status(201).json(newContact);
+  const { _id } = req.params;
+  const { name, email, phone, favorite } = req.body;
+  const validatedData = validateBody(req.body);
+  if (validatedData.error) {
+    return res.status(400).json({ status: validatedData.error });
+  }
+  const newContact = await Contact.create({
+    name,
+    email,
+    phone,
+    favorite,
+    owner: _id,
+  });
+  return res.status(201).json(newContact);
 }
 
 async function deleteContact(req, res, next) {
-    const { contactId } = req.params;
-    const contact = await Contact.findById(contactId);
-    if (!contact) {
-        return res.status(404).json({ message: 'Not found' });
-    }
-    await Contact.findByIdAndRemove(contactId);
-    return res.status(200).json({ "message": "contact deleted" });
-};
+  const { contactId } = req.params;
+  const contact = await Contact.findById(contactId);
+  if (!contact) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+  await Contact.findByIdAndRemove(contactId);
+  return res.status(200).json({ message: 'contact deleted' });
+}
 
 async function updateContact(req, res, next) {
-    const { contactId } = req.params;
-    const { favorite } = req.body;
-    const result = await Contact.findByIdAndUpdate({ _id: contactId }, { favorite: favorite }, { new: true });
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+  const result = await Contact.findByIdAndUpdate(
+    { _id: contactId },
+    { favorite: favorite },
+    { new: true }
+  );
 
-    return res.status(200).json(result);
-};
+  return res.status(200).json(result);
+}
+
+async function uploadImage(req, res, next) {
+  console.log('file', req.file);
+  const { filename } = req.file;
+
+  const tmpPath = path.resolve(__dirname, '../tmp', filename);
+  const newPath = path.resolve(__dirname, '../public', filename);
+
+  try {
+    await fs.rename(tmpPath, newPath);
+  } catch (error) {
+    await fs.unlink(tmpPath);
+    throw error;
+  }
+
+  const contactId = req.params.id;
+
+  const contact = await Contact.findById(contactId);
+  contact.image = `/public/${filename}`;
+  await contact.save();
+
+  return res.json({
+    data: { image: contact.image },
+  });
+}
 
 module.exports = {
-    getContacts,
-    getContactById,
-    deleteContact,
-    createContact,
-    updateContact,
+  getContacts,
+  getContactById,
+  deleteContact,
+  createContact,
+  updateContact,
+  uploadImage,
 };
