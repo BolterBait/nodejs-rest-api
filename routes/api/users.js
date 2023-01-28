1314dc80e70829b7fefad9431d3366856145e27d
BolterBait <osvyatobog@gmail.com>
2023-01-28 17:53:01 +0200

    step4 almost

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const { tryCatchWrapper } = require('../../helpers/index');
-const { auth } = require('../../middlewares/validator');
-const { createContact, getContacts, current } = require('../../controllers/users.controler');
-const { validateBody } = require('../../middlewares/validator');
+const { auth, upload, validateBody } = require('../../middlewares/validator');
+const {
+  createContact,
+  getContacts,
+  current,
+  uploadAvatar,
+} = require('../../controllers/users.controler');
 const { addContactSchema } = require('../../schemas');
-
 const usersRouter = express.Router();
 
-usersRouter.post("/contacts", tryCatchWrapper(auth),validateBody(addContactSchema), tryCatchWrapper(createContact));
-usersRouter.get("/contacts", tryCatchWrapper(auth), tryCatchWrapper(getContacts));
-usersRouter.get("/current", tryCatchWrapper(auth), tryCatchWrapper(current));
-
-module.exports = { usersRouter, };
-
+usersRouter.post(
+  '/contacts',
+  tryCatchWrapper(auth),
+  validateBody(addContactSchema),
+  tryCatchWrapper(createContact)
+);
+usersRouter.get(
+  '/contacts',
+  tryCatchWrapper(auth),
+  tryCatchWrapper(getContacts)
+);
+usersRouter.get('/current', tryCatchWrapper(auth), tryCatchWrapper(current));
+usersRouter.patch(
+  '/avatar',
+  upload.single('avatar'),
+  tryCatchWrapper(auth),
+  tryCatchWrapper(uploadAvatar)
+);
+module.exports = { usersRouter };
