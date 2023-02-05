b065ed675834a2ef9e5e0188ef8a7d3c85daf263
osvyatobog <osvyatobog@gmail.com>
2023-02-05 20:30:34 +0200

    sendgrid

--- helpers/index.js
+++ helpers/index.js
@@ -1,21 +1,35 @@
+const sendGrid = require('@sendgrid/mail');
+
 function tryCatchWrapper(endpointFn) {
-    return async (req, res, next) => {
-        try {
-            await endpointFn(req, res, next);
-        } catch (error) {
-            return next(error);
-        }
-    };
+  return async (req, res, next) => {
+    try {
+      await endpointFn(req, res, next);
+    } catch (error) {
+      return next(error);
+    }
+  };
 }
 
 class HttpError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "HttpError";
-    }
+  constructor(message) {
+    super(message);
+    this.name = 'HttpError';
+  }
+}
+
+async function sendMail() {
+  const email = {
+    from: 'alex4@ua.fm',
+    to: 'osvyatobog@gmail.com',
+    subject: 'Test',
+    html: '<h1>Hello world!</h1>',
+    text: 'Hello world!',
+  };
+
+  const response = await sendGrid.sendMail(email);
 }
 
 module.exports = {
-    tryCatchWrapper,
-    HttpError,
+  tryCatchWrapper,
+  HttpError,
 };
--- server.js
+++ server.js
@@ -1,23 +1,44 @@
+require('dotenv').config();
+const sendGrid = require('@sendgrid/mail');
 const mongoose = require('mongoose');
-const { app } = require('./app')
-const dotenv = require('dotenv');
+const { app } = require('./app');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 mongoose.set('strictQuery', false);
 
 const { HOST_URI } = process.env;
+const { SENDGRID_API_KEY } = process.env;
+
 (async function () {
   try {
     await mongoose.connect(HOST_URI);
 
-    console.log("Database connection successful");
+    console.log('Database connection successful');
     app.listen(3001, () => {
-      console.log("Server running. Use our API on port: 3001")
+      console.log('Server running. Use our API on port: 3001');
     });
   } catch (error) {
-    console.error('Main failed', error.message)
+    console.error('Main failed', error.message);
     process.exit(1);
   }
 })();
 
+async function main() {
+  sendGrid.setApiKey(SENDGRID_API_KEY);
+  try {
+    const email = {
+      from: 'alex4@ua.fm',
+      to: 'osvyatobog@gmail.com',
+      subject: 'Test',
+      html: '<h1>Hello world!</h1>',
+      text: 'Hello world!',
+    };
 
+    const response = await sendGrid.send(email);
+    console.log(response);
+  } catch (error) {
+    console.error('App error:', error);
+  }
+}
+main();
